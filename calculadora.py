4ff548fcec1c189f5b4d8213f3042fa0d49d1652
Pablo Girón <pgrtechdeveloper@gmail.com>
2016-09-22 01:44:51 -0700

    Add files via upload
    Una simple calculadora

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+print('Calculadora en Python') 
+num1 = int(input("Por favor, introduzca el primer número: "))
+while True:
+    operador = input('Introduzca el operador (+ , - , / , * : ')
+    if operador != '*' | operador != '/' | operador != '+' | operador != '-':
+        continue
+    else:
+ num2 = int(input("Por favor, introduzca el segundo número: "))
+
+
+
+
