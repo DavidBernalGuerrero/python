2a2615beee1985b54766bc4459279ba1382ae872
DavidBernalGuerrero <davidbg1992@hotmail.com>
2016-09-22 11:13:46 +0200

    Mejora al programa de prueba de Python

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,21 @@
-print('Calculadora en Python') 
-num1 = int(input("Por favor, introduzca el primer número: "))
-while True:
-    operador = input('Introduzca el operador (+ , - , / , * : ')
-    if operador != '*' | operador != '/' | operador != '+' | operador != '-':
-        continue
-    else:
- num2 = int(input("Por favor, introduzca el segundo número: "))
+print "Calculadora en Python" 
+num1 = raw_input("Por favor, introduzca el primer numero: ")
+operador = raw_imput('Introduzca el operador (+ , - , / , * :)')
+num2 = raw_input("Por favor, introduzca el segundo numero: ")
+if operador == '*' | operador == "/" | operador == '+' | operador == '-':
+if operador == '*':
+resultado = num 1 * num2
+else if operador = '/':
+resultado = num 1 / num2
+else if operador = '+':
+resultado = num 1 + num2
+else if operador = '-':
+resultado = num 1 - num2
+else:
+print('Operador Inválido')
+print "El resultado de la operación es: ", num1
+else:
+print "Operador Inválido"
 
 
 
